4bb1eb6b57603b1fcc56ce3880d376d8a1513914
iPonzu <joao_ignaciocastro1603@hotmail.com>
2026-03-27 21:40:47 -0300

    feat: adicionando elementos para Docker || atualizando banco de dados

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,18 +1,29 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI
+// const MONGO_URI = process.env.MONGO_URI
 
-if (!MONGO_URI) throw new Error("Mongo URI não definido. Verifique seu .env");
+// if (!MONGO_URI) throw new Error("Mongo URI não definido. Verifique seu .env");
+
+// export const connectiondb = async () => {
+//   try {
+//     await mongoose.connect(MONGO_URI);
+//     console.log('Database connected');
+//   } catch (err) {
+//     console.error("Falha ao se conectar ao banco", err);
+//     process.exit(1);
+//   }
+// };
+
+import mongoose from "mongoose";
 
 export const connectiondb = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
-    console.log('Database connected');
-  } catch (err) {
-    console.error("Falha ao se conectar ao banco", err);
-    process.exit(1);
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("Database connected");
+  } catch (error) {
+    console.error("Database connection error:", error);
   }
 };
