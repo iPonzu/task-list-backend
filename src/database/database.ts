97dc677b4bdc5f6331dd8f0f5027e36fbeaafbc6
iPonzu <joao_ignaciocastro1603@hotmail.com>
2026-03-27 21:53:18 -0300

    update: retirando elementos não utilizados

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,22 +1,3 @@
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const MONGO_URI = process.env.MONGO_URI
-
-// if (!MONGO_URI) throw new Error("Mongo URI não definido. Verifique seu .env");
-
-// export const connectiondb = async () => {
-//   try {
-//     await mongoose.connect(MONGO_URI);
-//     console.log('Database connected');
-//   } catch (err) {
-//     console.error("Falha ao se conectar ao banco", err);
-//     process.exit(1);
-//   }
-// };
-
 import mongoose from "mongoose";
 
 export const connectiondb = async () => {
--- src/models/taskList.ts
+++ src/models/taskList.ts
@@ -1,23 +1,3 @@
-// import mongoose, { Document, Schema } from "mongoose";
-// import { IUser } from "./taskUser";
-
-// export interface IList extends Document {
-//   title: string;
-//   user: IUser["_id"];
-// }
-
-// const TaskListSchema = new Schema<IList>(
-//   {
-//     title: { type: String, required: true },
-//     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-//   },
-//   { timestamps: true }
-// );
-
-// // evita erro de em ambiente de teste
-// export const TaskList =
-//   mongoose.models.TaskList || mongoose.model<IList>("TaskList", TaskListSchema);
-
 import mongoose, { Document, Schema } from "mongoose";
 import { IUser } from "./taskUser";
 
